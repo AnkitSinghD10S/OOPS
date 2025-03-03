868e943aef03f7601bffd3106fa8319ce0819998
ankitsingh <147189008+AnkitSinghD10S@users.noreply.github.com>
2025-03-03 20:24:11 +0530

    Add inheritance example with Base and Derived classes; update main method

--- Inheritance.java
+++ Inheritance.java
@@ -1,22 +1,24 @@
 public class Inheritance {
     public static void main(String[] args) {
-        InnerInheritance  o1 = new InnerInheritance ();
-        o1.name ="ankit";
-        o1.salary=100000000;
-        // System.out.println(o1.age);
-        // System.out.println(o1.name);
-        // System.out.println(o1.salary);
-        o1.show();
-        car c1 = new car();
-        c1.color="red";
-        c1.milegae=50;
-        c1.name="b6e";
-        c1.price=10000000;
-        c1.sunroof=false;
-        c1.display();
+        // InnerInheritance  o1 = new InnerInheritance ();
+        // o1.name ="ankit";
+        // o1.salary=100000000;
+        // // System.out.println(o1.age);
+        // // System.out.println(o1.name);
+        // // System.out.println(o1.salary);
+        // o1.show();
+        // car c1 = new car();
+        // c1.color="red";
+        // c1.milegae=50;
+        // c1.name="b6e";
+        // c1.price=10000000;
+        // c1.sunroof=false;
+        // c1.display();
 
-        C o2 = new C();
-        o2.run();
+        // C o2 = new C();
+        // o2.run();
+        Derived d = new Derived();
+        d.methodOne();
     }    
 }
 
@@ -87,5 +89,34 @@ interface b{
 class C implements a,b{
     public void  run(){
         System.out.println("C class run method");
+    }
+}
+
+class Base
+{
+    public void methodOne()
+    {
+        System.out.print("A");
+        methodTwo();
+    }
+
+    public void methodTwo()
+    {
+        System.out.print("B");
+    }
+}
+
+class Derived extends Base
+{
+    public void methodOne()
+    {
+        super.methodOne();
+        System.out.print("C");
+    }
+
+    public void method()
+    {
+        super.methodTwo();
+        System.out.print("D");
     }
 }
